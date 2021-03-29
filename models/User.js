db9ec4d2169247e3696b3ffa289e17821bb4198a
arnav-verma22 <arn.verma22@gmail.com>
2021-03-29 05:36:11 +0530

    user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const moment = require("moment");
+const userSchema = new Schema({
+    username: {
+        type : String,
+        required : true
+    },
+    email: {
+       type: String,
+       required : true,
+       unique : true 
+    },
+    password: {
+        type: String,
+        required : true
+    }
+    profile_pic: {
+        type: String,
+        default: "empty-avatar.jpg"
+    },
+    createdAt: {
+        type: String,
+        default: moment().format("DD/MM/YYYY") + ";" + moment.format("hh:mm:ss")
+    },
+    updatedAt: {
+        type: String,
+        default: moment().format("DD/MM/YYYY") + ";" + moment.format("hh:mm:ss")
+    }
+});
+
+mongoose.model("users", userSchema);
+mongoose.exports = mongoose.model("users");
