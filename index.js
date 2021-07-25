3987df58b83dff0d71c4e4e4823f412e61b71486
arnav-verma22 <arn.verma22@gmail.com>
2021-07-25 12:52:01 +0530

    setting up user routes in backend

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const morgan = require('morgan');
 const port = process.env.PORT;
 const database = require('./database');
 
+const userRoutes = require('./routes/users')
 
 app.use(cors());
 app.use(morgan('dev'));
 
+app.use('api/users', userRoutes);
 
 app.get('/', (req, res) => {
     return res.status(200).json({
