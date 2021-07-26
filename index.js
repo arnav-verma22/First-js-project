16cebce748b8acfba62dd02a086df68d748b528c
arnav-verma22 <arn.verma22@gmail.com>
2021-07-26 15:04:34 +0530

    trying few things

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.use(morgan('dev'));
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
+    var num1 = Number(req.body.num1);
+    console.log(num1);
+    return res.status(200).json({
+        'message' : "Welcome to Arnav's Homepage" 
+    });
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body.num1)
     return res.status(200).json({
         'message' : "Welcome to Arnav's Homepage" 
     });
